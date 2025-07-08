57b350b86aca14cc1a9a9173eb3c77a4a9a1e3a5
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-07-08 21:12:45 +0300

    Add project

---
+++ QuantumBookstore/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumBookstore
+{
+    public abstract class Book
+    {
+        public string ISBN { get;  }
+        public string Title { get; }
+        public int YearPublished { get; }
+        public double Price { get; }
+
+        protected Book(string isbn , string title , int yearPublished , double price)
+        {
+            ISBN = isbn;
+            Title = title;
+            YearPublished = yearPublished;
+            Price = price; 
+        }
+
+        public abstract void Buy(string isbn , int quantity , string email , string address);
+        public abstract bool IsAvailable(int quantity);
+
+    }
+}
